ca041a9922bfa46b783b5a417a3dafa1664f9f21
SzczepanJany <szczepan.jany@gmail.com>
2021-02-11 18:23:11 +0100

    Winning status function

--- main.py
+++ main.py
@@ -24,14 +24,28 @@ def ask_for_a_numbers_set(min_number,max_number):
     return result
 
 def choose_winning_numbers(min_number,max_number):
-    numbers = [str(i) for i in range(min_number,max_number)]    
+    numbers = [i for i in range(min_number,max_number)]    
     shuffle(numbers)
     rand_num = [numbers[i] for i in range(6)]
     result = sorted(rand_num)
-    return str(result)
+    return result
 
+def compare_results(user_input, computer_input):
+    result = []
+    for i in user_input:
+        if i in computer_input:
+            result.append(i)
+        
+    return result
 
 min_number = 1
 max_number = 49
-print(ask_for_a_numbers_set(min_number,max_number))
-print(choose_winning_numbers(min_number, max_number))
+user_in = ask_for_a_numbers_set(min_number,max_number)
+print(user_in)
+comp_in = choose_winning_numbers(min_number, max_number)
+print(comp_in)
+result = compare_results(user_in, comp_in)
+if len(result) > 2:
+    print(f'Yo won! You guessed {len(result)} : {result}')
+else:
+    print(f'Yo lost! You guessed only {len(result)} : {result}')
